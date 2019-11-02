90164f131732380f9a4cb38189a01dfe1e263242
KenanAL <kenan.rawashdah@gmail.com>
2019-11-02 17:06:04 +0200

    refactor api

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.use(express.json());
 
-app.get('/kenan', function (req, res) {
-  res.json({ name: 'kenan' })
+app.get("/", function(req, res) {
+  res.send("Hello World");
 });
 
-app.listen(3000, () => console.log('App listening on port 3000!'));
+let users = [{ id: "1fwefwef", name: "Kenan" }];
+
+app.get("/users", function(req, res) {
+  res.json(users);
+});
+
+app.delete("/user", function(req, res) {
+  if (req.query && req.query.userId) {
+    const userId = req.query.userId;
+    const tempArr = users.filter(function(user) {
+      return user.id !== userId;
+    });
+    users = tempArr;
+    res.json({ success: true });
+  } else {
+    res.json({ success: false, messsage: "Error: No userId" });
+  }
+});
+
+app.post("/user", function(req, res) {
+  users.push(req.body);
+  res.json({ success: true });
+});
+
+app.patch("/user", function(req, res) {
+  if (req.query && req.query.userId) {
+    const userId = req.query.userId;
+    users.forEach(function(user, index) {
+      if (user.id === userId) {
+        users[index].name = "nooor";
+      }
+    });
+    res.json({ success: true });
+  } else {
+    res.json({ success: false, messsage: "Error: No userId" });
+  }
+});
+
+app.listen(3000, () => console.log("App listening on port 3000!"));
